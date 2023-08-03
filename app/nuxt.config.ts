c8f2285e8a3337c96803ca683798c0ab959c619c
Zahid Akhyar <zahidakhyar@gmail.com>
2023-08-03 11:12:16 +0000

    feat (app): adding dependencies and newrelic

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      meta: [{ name: "description", content: "Zahid Akhyar Portofolio Site~" }],
+      script: [
+        {
+          hid: "NewRelic",
+          src: "/newrelic.js",
+          defer: true,
+          type: "text/javascript",
+        },
+      ],
+    },
+  },
+
+  ssr: false,
+
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+  ],
+
+  colorMode: {
+    preference: "system",
+    dataValue: "theme",
+    classSuffix: "",
+  },
+});
